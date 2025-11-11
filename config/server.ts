f965723de240430469d9a902ba6d5848ef8d35ec
jorge palis <palis963@hotmail.com>
2025-11-10 21:11:55 -0500

    inicio

---
+++ config/server.ts
@@ -0,0 +1,35 @@
+// config/env/production/server.ts
+
+// ¡Elimina la línea de importación!
+// import { defineConfig } from '@strapi/types'; // ELIMINA ESTO
+
+// Usa la función de flecha con el parámetro `env` tipado explícitamente como `any` (o el tipo correcto si lo tienes).
+export default ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  app: {
+    keys: env.array('APP_KEYS'),
+  },
+  webhooks: {
+    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+  },
+});
+
+
+// export default ({ env }) => ({
+//   host: env('HOST', '0.0.0.0'),
+//   port: env.int('PORT', 1337),
+//   // 🛑 LÍNEAS CLAVE: Habilitar la confianza en el proxy (TRUST_PROXY)
+//   url: env('PUBLIC_URL'),
+//   app: {
+//     keys: env.array('APP_KEYS'),
+//     proxy: true, 
+//     // Debes indicar a Strapi que confíe en los encabezados X-Forwarded-* (que usa Render)
+//     // Aseguramos que Strapi considere la conexión HTTPS
+//     //secure: true,
+//   },
+//   // Opcional, pero a veces necesario:
+//   webhooks: {
+//     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+//   },
+// });
